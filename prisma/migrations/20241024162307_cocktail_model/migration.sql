12931e8bc71559e80b7be9662dec6f1346824c91
GOLDER303 <szymonstepien@interia.pl>
2024-10-24 18:23:42 +0200

    Add cocktail model

---
+++ prisma/migrations/20241024162307_cocktail_model/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Cocktail" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "instructions" TEXT NOT NULL,
+    "image" TEXT
+);
