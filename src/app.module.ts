0dc8aa2a0315a1ec8cca26dd432c8f84f2e4acfb
GOLDER303 <szymonstepien@interia.pl>
2024-10-27 15:45:43 +0100

    Add cocktail and prisma module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { CocktailModule } from "./cocktail/cocktail.module";
+import { PrismaModule } from "./prisma/prisma.module";
 
 @Module({
-  imports: [],
+  imports: [CocktailModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
