d41304229915b37881328d842cc0a43f7152c46a
GOLDER303 <szymonstepien@interia.pl>
2024-10-27 16:56:01 +0100

    Generate ingredient module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { CocktailModule } from "./cocktail/cocktail.module";
 import { PrismaModule } from "./prisma/prisma.module";
+import { IngredientModule } from './ingredient/ingredient.module';
 
 @Module({
-  imports: [CocktailModule, PrismaModule],
+  imports: [CocktailModule, PrismaModule, IngredientModule],
   controllers: [AppController],
   providers: [AppService],
 })
