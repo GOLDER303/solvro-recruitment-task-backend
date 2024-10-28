5ab99f53548b88d7fb613423d86fcfdda02e6752
GOLDER303 <szymonstepien@interia.pl>
2024-10-28 20:00:54 +0100

    Create an endpoint to create ingredients

--- src/ingredient/ingredient.controller.ts
+++ src/ingredient/ingredient.controller.ts
@@ -1,8 +1,16 @@
 import {
+  Body,
   Controller,
   Get,
-  Param
+  Param,
+  Post,
+  Res,
+  UploadedFile,
+  UseInterceptors
 } from "@nestjs/common";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { Response } from "express";
+import { multerOptions } from "src/config/multer-options";
 import { IngredientService } from "./ingredient.service";
 
 @Controller("ingredient")
@@ -17,5 +25,22 @@ export class IngredientController {
   @Get(":id")
   async getIngredient(@Param("id") ingredientId: string) {
     return this.ingredientService.getIngredientById(+ingredientId);
+  }
+
+  @Post()
+  @UseInterceptors(FileInterceptor("image", multerOptions))
+  async createIngredient(
+    @UploadedFile() image: Express.Multer.File,
+    @Body() ingredientCreateDTO: IngredientCreateDTO,
+    @Res() res: Response,
+  ) {
+    const createdIngredient = await this.ingredientService.createIngredient({
+      ...ingredientCreateDTO,
+      image,
+    });
+
+    res.setHeader("Location", `/ingredient/${createdIngredient.id}`);
+
+    return res.status(201).send();
   }
 }
