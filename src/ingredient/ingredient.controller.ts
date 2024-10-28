aaa80df360cd8b1ff09feef5850865f40be91f68
GOLDER303 <szymonstepien@interia.pl>
2024-10-28 19:52:56 +0100

    Add an endpoint to get ingredients

--- src/ingredient/ingredient.controller.ts
+++ src/ingredient/ingredient.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from "@nestjs/common";
+import {
+  Controller,
+  Get,
+  Param
+} from "@nestjs/common";
+import { IngredientService } from "./ingredient.service";
 
 @Controller("ingredient")
-export class IngredientController {}
+export class IngredientController {
+  constructor(private readonly ingredientService: IngredientService) {}
+
+  @Get()
+  async getAllIngredient() {
+    return this.ingredientService.getAllIngredients();
+  }
+
+  @Get(":id")
+  async getIngredient(@Param("id") ingredientId: string) {
+    return this.ingredientService.getIngredientById(+ingredientId);
+  }
+}
