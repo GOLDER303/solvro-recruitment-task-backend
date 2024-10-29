46cc627ebabf7444157eb1c9148c52c9063dc525
GOLDER303 <szymonstepien@interia.pl>
2024-10-29 21:41:39 +0100

    WIP

--- src/cocktail/cocktail.controller.ts
+++ src/cocktail/cocktail.controller.ts
@@ -1,9 +1,20 @@
 import {
+  Body,
   Controller,
+  Delete,
   Get,
-  Param
+  Param,
+  Patch,
+  Post,
+  Res,
+  UseInterceptors,
 } from "@nestjs/common";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { Response } from "express";
+import { multerOptions } from "src/config/multer-options";
 import { CocktailService } from "./cocktail.service";
+import { CocktailCreateDTO } from "./dtos/cocktail-create.dto";
+import { CocktailPatchDTO } from "./dtos/cocktail-patch.dto";
 
 @Controller("cocktail")
 export class CocktailController {
@@ -17,5 +28,32 @@ export class CocktailController {
   @Get(":id")
   async getCocktail(@Param("id") cocktailId: string) {
     return this.cocktailService.getCocktailById(+cocktailId);
+  }
+
+  @Post()
+  @UseInterceptors(FileInterceptor("image", multerOptions))
+  async createIngredient(
+    @Body() ingredientCreateDTO: CocktailCreateDTO,
+    @Res() res: Response,
+  ) {
+    const createdCocktail =
+      await this.cocktailService.createCocktail(ingredientCreateDTO);
+
+    res.setHeader("Location", `/cocktail/${createdCocktail.id}`);
+
+    return res.status(201).send();
+  }
+
+  @Delete(":id")
+  async deleteCocktail(@Param("id") cocktailId: string) {
+    this.cocktailService.deleteCocktailById(+cocktailId);
+  }
+
+  @Patch(":id")
+  async patchCocktail(
+    @Param("id") cocktailId: string,
+    @Body() cocktailPatchDTO: CocktailPatchDTO,
+  ) {
+    return this.cocktailService.patchCocktail(+cocktailId, cocktailPatchDTO);
   }
 }
