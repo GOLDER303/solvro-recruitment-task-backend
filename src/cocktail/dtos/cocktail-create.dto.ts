6bf7bb86cdd40020c3f71ac731cff2c29e108fe0
GOLDER303 <szymonstepien@interia.pl>
2024-10-31 16:00:56 +0100

    Add validation to the create cocktail dto

--- src/cocktail/dtos/cocktail-create.dto.ts
+++ src/cocktail/dtos/cocktail-create.dto.ts
@@ -1,6 +1,35 @@
-export type CocktailCreateDTO = {
+import { Type } from "class-transformer";
+import {
+  IsArray,
+  IsInt,
+  IsNotEmpty,
+  IsString,
+  ValidateNested,
+} from "class-validator";
+
+class CreateCocktailDTOIngredient {
+  @IsInt()
+  ingredientId: number;
+
+  @IsString()
+  quantity: string;
+}
+
+export class CocktailCreateDTO {
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @IsNotEmpty()
+  @IsString()
   category: string;
+
+  @IsNotEmpty()
+  @IsString()
   instructions: string;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => CreateCocktailDTOIngredient)
   ingredients: { ingredientId: number; quantity: string }[];
-};
+}
